a8beb157ca81e115185d6f680533d22e4922e0d0
phamquangkhanh2999 <quangkhanh2999@gmail.com>
2022-04-02 10:22:24 +0700

    dialog-2

--- src/components/cart/DialogCart.jsx
+++ src/components/cart/DialogCart.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import "./DialogCart.style.scss";
+import { dataCart } from "../../data/dataCart";
+import numberWithCommas from "../../utils/numberWithCommas";
+import Image from "../../assets/images/product/product_01.jpg";
 
 class DialogCart extends Component {
   constructor(props) {
@@ -7,6 +10,7 @@ class DialogCart extends Component {
   }
   render() {
     const { isActiveDialog, isNavbar } = this.props;
+
     return (
       <div
         className={isActiveDialog ? "dialog-cart active-dialog" : "dialog-cart"}
@@ -29,12 +33,79 @@ class DialogCart extends Component {
               <i className='fa-solid fa-xmark'></i>&nbsp;&nbsp;Đóng
             </span>
           </div>
+          {dataCart && dataCart.length > 0 ? (
+            <div className='minicart-wrapper'>
+              {dataCart.map((item, index) => (
+                <div className='minicart-item' key={index}>
+                  <div className='minicart-item-info'>
+                    <div className='product-image-container'>
+                      <img src={item.image01} alt='' />
+                    </div>
+                    <div className='minicart-item-details'>
+                      <span className='minicart-item-name'>{item.title}</span>
+                      <div className='minicart-item-options'>
+                        <div className='minicart-item-option'>
+                          <span>Màu Sắc:&nbsp;</span>
+                          <span
+                            className='minicart-color'
+                            style={{ backgroundColor: `${item.colors}` }}
+                          ></span>
+                        </div>
+                        <div className='minicart-item-option'>
+                          <span className='minicart-size'>
+                            Kích cỡ:&nbsp; {item.size}
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='price-container'>
+                    <span className='minicart-price'>
+                      <strong> {numberWithCommas(item.price)}&nbsp;₫</strong>
+                    </span>
+                    <div className='minicart-item-qty'>
+                      <span className='decreasing-qty'>
+                        <i className='fa-solid fa-minus'></i>
+                      </span>
+                      <span className='item-qty'>1</span>
+                      <span className='increase-qty'>
+                        <i className='fa-solid fa-plus'></i>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              ))}
 
-          <div className='block-cart-wrapper'>
-            <p className='block-cart-text'>
-              Không có sản phẩm nào trong giỏ hàng của bạn
-            </p>
-          </div>
+              <div className='minicart-order'>
+                <div className='minicart-quantity'>
+                  <span>
+                    <strong>Tổng số sản phẩm: </strong>
+                  </span>
+                  <span>
+                    <strong>1</strong>
+                  </span>
+                </div>
+                <div className='minicart-subtotal'>
+                  <span>
+                    <strong>Tạm tính: </strong>
+                  </span>
+                  <span>
+                    <strong> {numberWithCommas(628000)}&nbsp;₫</strong>
+                  </span>
+                </div>
+                <div className='minicart-actions'>
+                  <div className='minicart-purchase'>Đặt hàng</div>
+                  <div className='minicart-viewcart'>Xem giỏ hàng</div>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div className='block-cart-wrapper'>
+              <p className='block-cart-text'>
+                Không có sản phẩm nào trong giỏ hàng của bạn
+              </p>
+            </div>
+          )}
         </div>
       </div>
     );
---
+++ src/data/dataCart/index.js
@@ -0,0 +1,28 @@
+const image_new_1 = require("../../assets/images/product/product_01.jpg");
+const image_new_2 = require("../../assets/images/product/product_02.jpg");
+export const dataCart = [
+  {
+    title: "ÁO SƠ MI - BA220401NT",
+    price: "638000",
+    productCode: "TS22352",
+    image01: image_new_1,
+    categorySlug: "ao-thun",
+    colors: "blue",
+    slug: "ao-thun-dinosaur-01",
+    size: "xl",
+    description:
+      "Áo sơ mi ngắn tay, form Body Fit dễ mặc, hợp form dáng.Áo sơ mi trẻ trung, hiện đại, dễ phối đồ.Áo sơ mi bạc hà kháng khuẩn tự nhiên, mát lạnh, mềm mượt, thân thiện với làn da.",
+  },
+  {
+    title: "ÁO SƠ MI - BA220421NT",
+    price: "538000",
+    productCode: "TS22352",
+    image01: image_new_2,
+    categorySlug: "ao-thun",
+    colors: "red",
+    slug: "ao-thun-dinosaur-01",
+    size: "l",
+    description:
+      "Áo sơ mi ngắn tay, form Body Fit dễ mặc, hợp form dáng.Áo sơ mi trẻ trung, hiện đại, dễ phối đồ.Áo sơ mi bạc hà kháng khuẩn tự nhiên, mát lạnh, mềm mượt, thân thiện với làn da.",
+  },
+];
